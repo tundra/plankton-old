9efae2f51a8971d2dfb919361b631710b6c8ca43
Christian Plesner Hansen <c7n@p5r.org>
2014-03-17 09:53:03 +0100

    Initial move from neutrino

---
+++ src/python/setup.py
@@ -0,0 +1,13 @@
+#- Copyright 2014 GOTO 10.
+#- Licensed under the Apache License, Version 2.0 (see LICENSE).
+
+import setuptools
+
+setuptools.setup(
+  name = "Plankton",
+  version = "0.0.1",
+  description = "Python support for the plankton format",
+  author = "GOTO 10",
+  url = "http://www.github.com/goto-10/plankton",
+  packages = setuptools.find_packages(),
+)
