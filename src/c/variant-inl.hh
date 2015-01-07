ae4613f0c160f00212ecac7651dd71a8e6287c2c
Christian Plesner Hansen <c7n@p5r.org>
2015-01-07 08:26:50 +0100

    Whoopsie

---
+++ src/c/variant-inl.hh
@@ -0,0 +1,49 @@
+//- Copyright 2015 the Neutrino authors (see AUTHORS).
+//- Licensed under the Apache License, Version 2.0 (see LICENSE).
+
+#ifndef _VARIANT_INL_HH
+#define _VARIANT_INL_HH
+
+#include "variant.hh"
+
+namespace plankton {
+
+template <typename T>
+inline T *Variant::native_get_value(ConcreteObjectType<T> *type) {
+  return type->cast(native_get_type(), native_get_raw_value());
+}
+
+template <typename T>
+T *ConcreteObjectType<T>::cast(AbstractObjectType *type, void *object) {
+  return (this == type) ? static_cast<T*>(object) : NULL;
+}
+
+template <typename T>
+Variant ObjectType<T>::get_initial_instance(Variant header, Factory *arena) {
+  T *instance = (create_)(header, arena);
+  return arena->new_native_object(this, instance);
+}
+
+template <typename T>
+Variant ObjectType<T>::get_complete_instance(Variant initial, Variant payload,
+    Factory *factory) {
+  T *value = initial.native_get_value(this);
+  if (value != NULL)
+    (complete_)(value, payload, factory);
+  return initial;
+}
+
+template <typename T>
+ObjectType<T>::ObjectType(Variant header, new_instance_t create, complete_instance_t complete)
+  : header_(header)
+  , create_(create)
+  , complete_(complete) { }
+
+} // namespace plankton
+
+// Allocate a block of memory within the given factory.
+inline void *operator new(size_t size, plankton::Factory &factory) {
+  return factory.alloc_raw(size);
+}
+
+#endif // _PLANKTON_HH
